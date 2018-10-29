40010d0aa7d357a28d3e376697afbc7921528b6a
Majid Hajian <mhadaily@gmail.com>
2018-10-29 15:10:45 +0100

    migrate to new blog

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('/Users/majid/Projects/Personal/mhadaily.github.io/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/majid/Projects/Personal/mhadaily.github.io/node_modules/gatsby-plugin-catch-links/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/majid/Projects/Personal/mhadaily.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-browser'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,18 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-templates-blog-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/templates/Blog.tsx" /* webpackChunkName: "component---src-templates-blog-tsx" */),
+  "component---src-templates-all-category-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/templates/AllCategory.tsx" /* webpackChunkName: "component---src-templates-all-category-tsx" */),
+  "component---src-templates-category-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/templates/Category.tsx" /* webpackChunkName: "component---src-templates-category-tsx" */),
+  "component---src-templates-all-tag-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/templates/AllTag.tsx" /* webpackChunkName: "component---src-templates-all-tag-tsx" */),
+  "component---src-templates-tag-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/templates/Tag.tsx" /* webpackChunkName: "component---src-templates-tag-tsx" */),
+  "component---src-templates-post-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/templates/Post.tsx" /* webpackChunkName: "component---src-templates-post-tsx" */),
+  "component---cache-dev-404-page-js": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
+  "component---src-pages-contact-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/pages/contact.tsx" /* webpackChunkName: "component---src-pages-contact-tsx" */),
+  "component---src-pages-index-tsx": () => import("/Users/majid/Projects/Personal/mhadaily.github.io/src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
+}
+
+exports.data = () => import("/Users/majid/Projects/Personal/mhadaily.github.io/.cache/data.json")
+
